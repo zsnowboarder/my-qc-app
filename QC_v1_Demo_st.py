65e8783abb715d099a08e906f0e59ac61f832a62
zsnowboarder <146484453+zsnowboarder@users.noreply.github.com>
2024-10-30 21:36:19 -0700

    QC_v1_Demo_st.py

--- QC_v1_Demo_st.py
+++ QC_v1_Demo_st.py
@@ -24,9 +24,22 @@ with open("/mount/src/my-qc-app/model_svm.pkl", "rb") as file:
 
 # create interface
 
-st.title("QC Classifier")
+col1, col2, col3 = st.columns(1,5,1)
 
-new_data = st.text_input("Text", "I was walking and someone hit me.")
+intro = """This demo showcases a simple yet powerful tool designed for classifying police reports using machine learning.\n
+Despite being a very basic model trained on just 40 data points without any text pre-processing, feature engineering, and model fine-tuning, 
+it still achieves impressive classification results. Imagine the potential this tool when fully optimized 
+and trained on more extensive data. Machine learning can streamline the process of organizing reports, saving 
+valuable time and resources. It’s designed to be user-friendly and efficient, making it an invaluable asset for police departments."""
+
+st.title("QC Classifier")
+st.write('')
+st.write(intro)
+st.write('')
+st.write('This model can classify assaults, thefts, TFA, BNE, and robberies. Offences outside of these categories will be classified with a low probability indicator.')
+
+with col2:
+    new_data = st.text_input("Text", "I was walking and someone punched me for no reason. I had minor injuries. I reported the incident to police.")
 
 if st.button("Classify"):
     new_data = [new_data]
