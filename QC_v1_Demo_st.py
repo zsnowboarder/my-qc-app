5e38bb3104d81fbbdc51a62f093b486add70dcfa
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-10-31 19:16:47 -0700

    QC_v1_Demo_st.py

--- QC_v1_Demo_st.py
+++ QC_v1_Demo_st.py
@@ -45,7 +45,7 @@ new_data = st.text_area("Enter a synopsis. The more text entered, the better the
 if st.button("Classify"):
     new_data = [new_data]
     new_data_vect = vect_tfidf.transform(new_data)
-    new_pred = svm_model.predict(new_data_vect)
+    new_pred = svm_model.predict(new_data)
     pred_prob = svm_model.predict_proba(new_data)[0]
     pred_prob = round(max(pred_prob)*100)
 
