802798cebf084b72bce09c3d4792a55b9c83a43b
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-10-31 22:43:53 -0700

    QC_v1_Demo_st.py

--- QC_v1_Demo_st.py
+++ QC_v1_Demo_st.py
@@ -117,7 +117,7 @@ if st.button("Classify"):
     st.write('More details based on my training.')
     
     for label, prob in zip(class_labels, pred_prob):
-        st.write(f"Class: {label}, Probability: {prob * 100:.2f}%")
+        st.write(f"Category: {label},, Probability: {prob * 100:.2f}%")
     
 
 
