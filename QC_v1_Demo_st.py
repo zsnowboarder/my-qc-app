91f3296600e94268d623d4fa9100ab51da4abac4
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-11-01 08:24:50 -0700

    QC_v1_Demo_st.py

--- QC_v1_Demo_st.py
+++ QC_v1_Demo_st.py
@@ -106,7 +106,7 @@ if st.button("Classify"):
     if highest_prob > 30:
         pred_msg = "I am " + str(highest_prob) + "% confident that this can be classified as " + new_pred_highest + "."
     
-    elif highest_prob > 25:
+    elif highest_prob > 20:
         pred_msg = "Since I was trained on only an **extremely small dataset**, I will have to make an inference on your text input. It would be " + new_pred_highest + " or " + new_pred_second + " or both."
     
     else: 
