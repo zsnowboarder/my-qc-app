7bd2ac96a1b066e1abca63f7ad0d5c086f104a1a
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-10-31 21:38:08 -0700

    QC_v1_Demo_st.py

--- QC_v1_Demo_st.py
+++ QC_v1_Demo_st.py
@@ -93,7 +93,7 @@ if st.button("Classify"):
     pred_msg = ""
     # set the result to a label
     if highest_prob > 50:
-        pred_msg = "I am " + str(pred_prob) + "% confident that this can be classified as " + new_pred[0] + "."
+        pred_msg = "I am " + str(highest_prob) + "% confident that this can be classified as " + new_pred[0] + "."
     
     elif highest_prob > 25:
         pred_msg = "Since I was only trained on only an extremely small dataset, I will provide two possibilties on something I have not been trained on. In this case either " + new_pred[0] + " or " + new_pred_second
