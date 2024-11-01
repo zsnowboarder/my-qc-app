3c14ef367452a40d9fe9548965705d6857a2e9eb
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-10-31 23:02:48 -0700

    QC_v1_Demo_st.py

--- QC_v1_Demo_st.py
+++ QC_v1_Demo_st.py
@@ -103,10 +103,10 @@ if st.button("Classify"):
     # clear the pred text label
     pred_msg = ""
     # set the result to a label
-    if highest_prob > 40:
+    if highest_prob > 30:
         pred_msg = "I am " + str(highest_prob) + "% confident that this can be classified as " + new_pred_highest + "."
     
-    elif highest_prob > 20:
+    elif highest_prob > 25:
         pred_msg = "Since I was only trained on only an **extremely small dataset**, I will provide two possibilties on something I have not been trained on. In this case either " + new_pred_highest + " or " + new_pred_second
     
     else: 
